2cce0fc50af49fcb802d419cc7449a6e4063ee9b
sylar <sylar5210@gmail.com>
2019-04-18 16:21:36 +0900

    threading added

--- AnomalyCases/readAnomalies.py
+++ AnomalyCases/readAnomalies.py
@@ -2,7 +2,7 @@ import pygsheets
 import pandas as pd
 import os
 from datetime import datetime, timedelta
-from collect_data import dataWriter
+from tools import dataWriter, ThreadPool
 
 save_address = '../results_anomalies/'
 time_window = 3
@@ -42,17 +42,27 @@ def writeAnomalies(boundary, clear=True):
     if clear:
         clearAnomalies()
     anomalies = readAnomalies(boundary)
+    pool = ThreadPool(4)
+    tasks = []
     for anomaly in anomalies:
         print(anomaly[0])
         t1, t2 = calculateTimeWindow(anomaly[1], anomaly[2])
         print(t1)
         print(t2)
-        dw = dataWriter(anomaly[0], t1, t2, save_address=save_address)
-        dw.collect_main(False)
+        tasks.append([anomaly[0], t1, t2, save_address])
+    pool.map(writingWorker, tasks)
+    pool.wait_completion()
+        # dw = dataWriter(anomaly[0], t1, t2, save_address=save_address)
+        # dw.main(False)
 
 def calculateTimeWindow(t1, t2):
     return str(datetime.strptime(t1, '%Y-%m-%d')-timedelta(days=time_window))[:10],\
            str(datetime.strptime(t2, '%Y-%m-%d')+timedelta(days=time_window))[:10]
 
-writeAnomalies('A2:E12')
-writeAnomalies('A14:E20')
+def writingWorker(params):
+    dw = dataWriter(params[0], params[1], params[2], params[3])
+    dw.main(False)
+
+if __name__ == '__main__':
+    writeAnomalies('A2:E12')
+    # writeAnomalies('A14:E20')
--- tools.py
+++ tools.py
@@ -1,3 +1,122 @@
+from ihr.hegemony import Hegemony
+import os
+from datetime import datetime,timedelta
+import shutil
+from threading import Thread
+from queue import Queue
+
+save_address= './results/'
+
+class Worker(Thread):
+    """ Thread executing tasks from a given tasks queue """
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try:
+                func(*args, **kargs)
+            except Exception as e:
+                # An exception happened in this thread
+                print(e)
+            finally:
+                # Mark this task as done, whether an exception happened or not
+                self.tasks.task_done()
+
+
+class ThreadPool:
+    """ Pool of threads consuming tasks from a queue """
+    def __init__(self, num_threads):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads):
+            Worker(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        """ Add a task to the queue """
+        self.tasks.put((func, args, kargs))
+
+    def map(self, func, args_list):
+        """ Add a list of tasks to the queue """
+        for args in args_list:
+            self.add_task(func, args)
+
+    def wait_completion(self):
+        """ Wait for completion of all the tasks in the queue """
+        self.tasks.join()
+
+class dataWriter(object):
+    def __init__(self, originasns, day1, day2, save_address=save_address):
+        self.hegeDict = {}
+        self.HegeDict = {}
+        self.originasns = originasns
+        self.startdate = day1
+        self.enddate = day2
+        self.save_address = save_address
+        if not os.path.exists(self.save_address):
+            os.mkdir(self.save_address)
+        if not os.path.exists(self.save_address+'/'+str(self.originasns)):
+            os.mkdir(self.save_address+'/'+str(self.originasns))
+        self.save_address = self.save_address+str(self.originasns)+'/'
+
+    def main(self, clear=True):
+        count = 0
+        if clear:
+            self.clear()
+        d1 = datetime.strptime(self.startdate, '%Y-%m-%d')
+        d2 = datetime.strptime(self.enddate, '%Y-%m-%d')+timedelta(days=1)
+        while d1 != d2:
+            count += 96
+            self.collect(str(d1)[:10])
+            d1 += timedelta(days=1)
+        for key in self.HegeDict.keys():
+            with open(self.save_address+key, mode='w+') as output:
+                data = self.HegeDict[key]
+                if len(data)<count:
+                    data.extend(['0']*(count-len(data)))
+                output.write('\n'.join(self.HegeDict[key])+'\n')
+
+    def collect(self, day):
+        if os.path.exists(self.save_address+day):
+            print(1)
+            return
+        else:
+            print(0)
+            save_address = self.save_address+day+'/'
+            os.mkdir(self.save_address+day+'/')
+            hege = Hegemony(originasns=self.originasns, start=day+' 00:00', end=day+' 23:59')
+            for r in hege.get_results():
+                for data in r:
+                    key = str(data['originasn']) + '_' + str(data['asn'])
+                    if key in self.hegeDict.keys():
+                        self.hegeDict[key].append(str(data['hege']))
+                    else:
+                        self.hegeDict[key] = [str(data['hege'])]
+            self.write(save_address)
+
+    def write(self, save_address):
+        for key in self.hegeDict.keys():
+            if len(self.hegeDict[key])<96:
+                self.hegeDict[key].extend(['0']*(96-len(self.hegeDict[key])))
+            with open(save_address+key, mode='w+') as output:
+                output.write('\n'.join(self.hegeDict[key])+'\n')
+        self.aggregate()
+
+    def aggregate(self):
+        for key in self.hegeDict.keys():
+            if key in self.HegeDict.keys():
+                self.HegeDict[key].extend(self.hegeDict[key])
+            else:
+                self.HegeDict[key] = self.hegeDict[key]
+        self.hegeDict.clear()
+
+    def clear(self):
+        shutil.rmtree(self.save_address)
+        os.mkdir(self.save_address)
+
 class recordWriter(object):
     def __init__(self, filename):
         self.outputs = []
